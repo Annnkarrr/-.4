fc655e31055f8b7e369fb7a9dd9cadd2c6885050
Карякина Настя <nakarykina04@mail.ru>
2024-01-22 19:06:40 +0300

    добавила решение Task_001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при 
+//вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+
+
+
+int CalculateSum(int Number)
+{
+    int sum = 0;
+    while ( Number != 0)
+    {
+        int digit = Number % 10;
+        sum += digit;
+        Number /= 10;
+    }
+    return sum;
+}
+
+
+    while (true)
+    {
+        Console.WriteLine("Введите целое число: ");
+        string userInput = Console.ReadLine();
+
+        if (userInput == "q")
+            break;
+
+        int Number = Convert.ToInt32(userInput);
+        if ( CalculateSum(Number) % 2 == 0)
+            Console.WriteLine("Сумма чисел четная. Вы выходите из системы!!!");
+            break;
+    }
+
+
+
