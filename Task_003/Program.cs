86c51c0fc28284ad02854d30cd9b2f4067a6c2ce
Карякина Настя <nakarykina04@mail.ru>
2024-01-29 18:55:22 +0300

    добавила решение Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+
+int[] array = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+// Переворачиваем массив
+ReverseArray(array);
+
+Console.WriteLine("Перевернутый массив:");
+PrintArray(array);
+
+Console.ReadLine();
+
+
+// Метод для переворачивания массива
+static void ReverseArray(int[] array)
+{
+    int left = 0, right = array.Length - 1;
+
+    while (left < right)
+    {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+
+        left++;
+        right--;
+    }
+}
+
+// Метод для вывода массива
+static void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
